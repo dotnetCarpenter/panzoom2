b3564ed5b81a22db844e73a6628afdb8b3cb3809
dotnetCarpenter <jon.ronnenberg@gmail.com>
2018-04-01 12:54:27 +0200

    cleanup

--- src/gestures/Pinch.js
+++ src/gestures/Pinch.js
@@ -141,20 +141,4 @@ function normalizeEvent(ev) {
   return event
 }
 
-function diff (event1, event2) {
-  // https://stackoverflow.com/questions/2264072/detect-a-finger-pinch-through-javascript-on-the-iphone-and-android
-  const deltaX = event1.touches[0].x - event2.touches[0].x // TODO: make addition and substraction easier for Point
-  const deltaY = event1.touches[0].y - event2.touches[0].y
-
-  // TODO: return an enum instead
-  // console.log(deltaX, deltaY)
-  if (Math.abs(deltaX) > Math.abs(deltaY)) {
-    if (deltaX > 0) return 'left'
-    else return 'right'
-  } else {
-    if (deltaY > 0) return 'up'
-    else return 'down'
-  }
-}
-
 export default Pinch
