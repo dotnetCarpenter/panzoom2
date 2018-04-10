ebf1a73416a5d94e2b45758f04ec908316c3de26
dotnetCarpenter <jon.ronnenberg@gmail.com>
2018-04-10 22:33:38 +0200

    vue-like

--- src/index.js
+++ src/index.js
@@ -7,19 +7,15 @@ import Pan from './gestures/Pan'
 import Wheel from './gestures/Wheel'
 
 import Zoom from './referents/Zoom'
-import Move from './referents/Move'
+// import Move from './referents/Move'
 
-const referents = new Map([
-  ['zoom', () => new Zoom()],
-  ['move', () => new Move()]
-])
+
 
 export {
   createPanzoom,
   PanZoom, // only exposed to test
   Translate3d, // only exposed to test
   Observer, // only exposed to test
-  referents,
   Pinch,
   Pan,
   Wheel
--- src/referents/Zoom.js
+++ src/referents/Zoom.js
@@ -1,7 +1,27 @@
-class Zoom {
-  constructor () {}
+import Pinch from '../gestures/Pinch'
+import Wheel from '../gestures/Wheel'
 
-  zoom (point, multiplier) {}
+export default {
+  gestures: {
+    Pinch,
+    Wheel
+  },
+
+  // required custom properties from option object
+  props: {},
+
+  // life cycle handlers
+  setup () {
+    this.$el.on('pinch')
+  },
+  moveHandler () {},
+  endHandler () {},
+  unlisten () {},
+  destroy () {},
+
+  // public methods to expose
+  methods: {
+    zoom (point, multiplier) {}
+  }
+
 }
-
-export default Zoom
